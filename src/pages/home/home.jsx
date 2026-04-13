cf701ffe24959b2b231e061a9932b0e5c17adefb
jitendratpo <subhash@travelportalonline.com>
2026-04-13 17:08:27 +0530

    Fixing Homemid case sensitivity for Vercel

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Navbar } from '../../component/navbar'
 import { Footer } from '../../component/footer'
-import { Homemid } from './Homemid'
+import { Homemid } from './homemid'
 
 
 
